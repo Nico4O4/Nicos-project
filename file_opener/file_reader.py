7472d845a1c45b7de20e3d31149968ec12a8b584
Nico4O4 <nicopfin@bluewin.ch>
2025-12-01 18:22:49 +0100

    Updated my file reader |  It has now a Menu | WIP

--- file_opener/file_reader.py
+++ file_opener/file_reader.py
@@ -1,17 +1,57 @@
 import pyinputplus as pyip
-
-print("File opener from Nico4O4")
-print("\nInsert path to check content")
-
-user_input_file_path = pyip.inputFilepath(prompt=">> ", mustExist=True)
+import sys
 
 
-try:
-    file = open(user_input_file_path, "r")
-    print("\n--Current file content--\n", file.read())
+def file_reader():
+    user_input_file_path = pyip.inputFilepath(prompt="Path to file >> ", mustExist=True)
+    
+    try:
+        file = open(user_input_file_path, "r")
+        print("\n--Current file content--", "\n\n", file.read(), "\n\n")
+    
+    except:
+        FileNotFoundError
+        print("File not found")
 
-except:
-    FileNotFoundError
-    print("File not found")
+
+#first menu when starting programm
+def first_menu():
+    while True:
+        print("\nFile reader | made by Nico4O4")
+        print("'r' | read a file via path")
+        print("'q' | Exit")
+
+        user_input1 = input("--> ")
+        
+        if user_input1 == "r".lower():
+            file_reader()
+            break
+
+        if user_input1 == "q".lower():
+            sys.exit()
+
+        else:
+            print("Invalid input\n")
+            first_menu()
+        
+#----------------------------------
+first_menu()
 
 
+def menu_after_first_interaction():
+    while True:
+        print("'r' | read another file")
+        print("'q' | Exit")
+        user_input2 = input("--> ")
+    
+        if user_input2 == "r".lower():
+            file_reader()
+
+        if user_input2 == ("q").lower():
+            sys.exit()
+    
+        else:
+            print("Invalid input\n")
+            menu_after_first_interaction()
+
+menu_after_first_interaction()
