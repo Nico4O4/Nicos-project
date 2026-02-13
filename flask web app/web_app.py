d215bafcd9200d08df1c7425303d5bd677ec3682
Nico4O4 <nicopfin@bluewin.ch>
2026-02-13 16:39:54 +0100

    Delete flask web app directory

--- flask web app/web_app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask #flask importieren
-
-meine_app = Flask(__name__) #flask app erstellen (instanz)
-
-@meine_app.route("/") #route -> für die route welche URL flask nutzen soll zzb. site/home/login etc.
-def hello_world(): #nachricht im browser anzeigen
-    return "<p>Haii World</p>" #nachricht in HTML format als string
