fd8cfb2184b73e7979964812f198ca94873581eb
Nico4O4 <nicopfin@bluewin.ch>
2026-05-09 01:58:41 +0200

    still doesnt work... im gonna commit anyways

--- OOP Projects/oop_dict.py
+++ OOP Projects/oop_dict.py
@@ -1,22 +1,28 @@
 class  Myclass:
-    def __init__(self, save, show):
+    def __init__(self, user_input_taker, sec_input_taker):
+        #self.irgendwas (irgendwas) ist die variable mit der tatsächlichen info
 
-        fav_animes = {
-        "name": self.answer_var 
-        }
+        self.take_input = user_input_taker
+        self.take_input = None
 
-        self.answer_var = save #answer_var wo es tatsächlich gespeichert wird
-        self.show_var = show
+        if self.take_input is False:
+            self.take_sec_input = sec_input_taker
 
-
-        self.answer_var["name"] = self.answer_var
+    
+        animes =  {"absolute_fav_anime": self.take_input, "second_fav_anime": self.take_sec_input}   
         
-        self.show_var = fav_animes.get("name")
-
-        #printen
-        print("Your answer: ", self.show_var)
         
-user_input = Myclass(input("Whats your fav anime? "))
+        print(animes)
+
+def menu():
+    user_input = Myclass(input("Answer "))
+    user_input1 = Myclass(input("Answer "))
 
 
 
+if __name__ == "__main__":
+    menu()
+
+#--notes--
+#p1 = Person("Linus", 28) - linus die erste variable in der class 28 die zweite variable man fügt das nacheinander ein
+#irgendwie den nächsten input in die nächste var speichern aber wie???
